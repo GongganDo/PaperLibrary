c3740139ccd05cdf63d3c8424eb4eb83f21a8fd5
Sunghyun Kang <asdf@ec.cse.cau.ac.kr>
2014-10-21 16:49:12 +0900

    add CountMap

---
+++ src/net/caucse/paperlibrary/CountMap.java
@@ -0,0 +1,22 @@
+package net.caucse.paperlibrary;
+
+import java.util.HashMap;
+
+public class CountMap<K> extends HashMap<K, Integer> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3751739822642300612L;
+	
+	public int add(K key) {
+		if (this.containsKey(key)) {
+			int value = this.get(key);
+			this.put(key, ++value);
+			return value;
+		} else {
+			this.put(key, 1);
+			return 1;
+		}
+	}
+}
