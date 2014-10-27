086a99764641edb9f83d52e47cb57ce28fb246c3
Sunghyun Kang <asdf@ec.cse.cau.ac.kr>
2014-10-27 19:17:01 +0900

    use ScoreMap instead of HashMap

--- src/net/caucse/paperlibrary/ScoreMap.java
+++ src/net/caucse/paperlibrary/ScoreMap.java
@@ -9,13 +9,26 @@ public class ScoreMap<K> extends HashMap<K, Double> {
 	 */
 	private static final long serialVersionUID = 8508325982320215960L;
 	public double add(K key) {
+		return add(key, 1.0);
+	}
+	public double add(K key, double addingValue) {
 		if (this.containsKey(key)) {
 			double value = this.get(key);
-			this.put(key, ++value);
+			this.put(key, value += addingValue);
 			return value;
 		} else {
-			this.put(key, 1.0);
-			return 1.0;
+			this.put(key, addingValue);
+			return addingValue;
+		}
+	}
+	public double mul(K key, double mulValue) {
+		if (this.containsKey(key)) {
+			double value = this.get(key);
+			this.put(key, value *= mulValue);
+			return value;
+		} else {
+			this.put(key, mulValue);
+			return mulValue;
 		}
 	}
 }
